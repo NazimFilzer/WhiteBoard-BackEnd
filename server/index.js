2d2f6c26e1213e7a59c7b6549ab712716992cabb
NzM <nazimfilzer@gmail.com>
2022-10-18 18:34:17 +0530

    Bot is Ready

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000","collaber.netlify.app"],
+    origin: ["http://localhost:3000", "collaber.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
@@ -23,7 +23,20 @@ io.on("connection", (socket) => {
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    socket.to(data.room).emit("back", "Hello")
+    socket.join(data.room);
+    console.log(`User with ID: ${socket.id} and username: ${data.username} joined room: ${data.room}`);
+    socket.data.username = data.username;
+    socket.data.room = data.room;
+    const messageData = {
+      room: data.room,
+      author: 'Bot',
+      message: `${data.username} has joined the room.`,
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
+    };
+    socket.to(data.room).emit("receive_message", messageData);
   });
 
   socket.on("send_message", (data) => {
@@ -32,18 +45,28 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
+    const messageData = {
+      room: socket.data.room,
+      author: 'Bot',
+      message: `${socket.data.username} has disconnected from the room.`,
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
+    };
+    socket.to(socket.data.room).emit("receive_message", messageData);
   });
-  
+
   socket.on('canvas-data', (data) => {
     socket.broadcast.emit('canvas-data', data);
 
   });
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('testing')
 })
 
-server.listen(process.env.PORT ||3001 , () => {
+server.listen(process.env.PORT || 3001, () => {
   console.log("SERVER RUNNING");
 });
