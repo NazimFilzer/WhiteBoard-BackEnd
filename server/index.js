a7a352aa82238ac7b3521563d4d703fd952efd24
NzM <nazimfilzer@gmail.com>
2022-10-21 22:12:18 +0530

    Chat Issue Fixed

--- server/index.js
+++ server/index.js
@@ -19,26 +19,27 @@ let roomsData = {} //tracks all room data
 //example : roomsData = {'12345' : {'count' : 5, 'members' : set of users}}
 
 
-let mostRecentCanvasData;
+let mostRecentCanvasData = {} //tracks all room most recent canvas Data
+//example : mostRecentCanvasData = {'12345' : canvasData}
 
 io.on("connection", (socket) => {
   //adding this new connection to connections set
   connections.add(socket);
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", async(data) => {
-    
+  socket.on("join_room", async (data) => {
+
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
     let roomId = data.room
     socket.join(roomId);
-    
+
     await handleRoomJoin(socket, roomId);
     io.to(roomId).emit('members', roomsData[roomId])
 
-    if (mostRecentCanvasData) {
-      io.to(roomId).emit('receive_canvas_data', mostRecentCanvasData)
+    if (mostRecentCanvasData[roomId]) {
+      io.to(roomId).emit("receive_canvas_data", mostRecentCanvasData[roomId]);
     }
-    
+
     console.log(`User with ID: ${socket.id} and username: ${data.username} joined room: ${data.room}`);
     socket.data.username = data.username;
     socket.data.room = data.room;
@@ -58,7 +59,7 @@ io.on("connection", (socket) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
-  socket.on("disconnect", async() => {
+  socket.on("disconnect", async () => {
     console.log("User Disconnected", socket.id);
     await handleRoomLeave(socket);
     const messageData = {
@@ -72,9 +73,9 @@ io.on("connection", (socket) => {
     };
     socket.to(socket.data.room).emit("receive_message", messageData);
   });
-  
+
   socket.on('send_canvas_data', (data) => {
-    mostRecentCanvasData = data.canvas;
+    updateMostRecentCanvasData(data.room, data.canvas);
     socket.to(data.room).emit('receive_canvas_data', data.canvas);
 
   });
@@ -86,17 +87,17 @@ io.on("connection", (socket) => {
  * @param {*} socket instance of socket,
  * @param {*} roomId id of the room
  */
-const handleRoomJoin = async(socket, roomId) => {
+const handleRoomJoin = async (socket, roomId) => {
   let roomIdExists = roomsData[roomId] !== undefined;
-  if(roomIdExists){
+  if (roomIdExists) {
     let targetRoomData = roomsData[roomId];
     targetRoomData['count'] += 1;
     targetRoomData['members'].add(socket);
     roomsData[roomId] = targetRoomData;
-  }else{
+  } else {
     let members = new Set();
     members.add(socket);
-    let tempData = {'count':1, 'members': members};
+    let tempData = { 'count': 1, 'members': members };
     roomsData[roomId] = tempData;
   }
 }
@@ -106,13 +107,13 @@ const handleRoomJoin = async(socket, roomId) => {
  * when a user disconnects, delete that user from all rooms in which he was joined earlier
  * @param {*} socket instance of socket,
  */
-const handleRoomLeave = async(socket) => {
+const handleRoomLeave = async (socket) => {
   //delete from connection set
   connections.delete(socket);
   //update roomData;
-  for(let key in roomsData){
+  for (let key in roomsData) {
     let currRoomData = roomsData[key];
-    if(currRoomData['members'].has(socket)){
+    if (currRoomData['members'].has(socket)) {
       currRoomData['members'].delete(socket);
       currRoomData['count'] -= 1;
       roomsData[key] = currRoomData;
@@ -122,6 +123,13 @@ const handleRoomLeave = async(socket) => {
 
 }
 
+const updateMostRecentCanvasData = (roomId, data) => {
+  mostRecentCanvasData[roomId] = data;
+}
+
+const getCanvasData = (roomId) => {
+  return mostRecentCanvasData[roomId];
+}
 
 app.get('/', (req, res) => {
   res.send('testing')
